524b3b8d9615e404861b03d56069171a52affd50
Arnold Herrera <arnoldherrera@cbit-online.com>
2019-05-26 20:12:36 -0500

    Actaulización Project

--- build/generated-sources/ap-source-output/com/afnemo/dto/Persona_.java
+++ build/generated-sources/ap-source-output/com/afnemo/dto/Persona_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-12T19:56:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-25T01:02:59")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
